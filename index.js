7cc3828f7b58b93918d5431b8a47f4e67db339da
theJian <thejianmail@gmail.com>
2016-10-05 00:08:29 +0800

    Add Promise type

---
+++ index.js
@@ -0,0 +1,29 @@
+function Promise(fn) {
+  var state = 'pending';
+  var value;
+  var deferred;
+
+  function resolve(newValue) {
+    value = newValue;
+    state = 'resolved';
+
+    if(deferred) {
+      handle(deferred);
+    }
+  }
+
+  function handle(handler) {
+    if(state === 'pending') {
+      deferred = handler;
+      return;
+    }
+
+    handler(value);
+  }
+
+  this.then = function (onResolved) {
+    handle(onResolved);
+  };
+
+  fn(resolve);
+}
